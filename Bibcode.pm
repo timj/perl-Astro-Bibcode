3c74a6975f53991569bdc57fd95dfda040904aa2
Tim Jenness <t.jenness@jach.hawaii.edu>
2007-05-21 23:36:08 +0000

    svn and cvs versions differ so code to extract Revision string needs to change
    svn path=/trunk/perlmods/; revision=11312

--- Bibcode.pm
+++ Bibcode.pm
@@ -28,7 +28,7 @@ use warnings::register;
 use Carp;
 
 use vars qw/ $VERSION /;
-$VERSION = sprintf("%d.%03d", q$Revision$ =~ /(\d+)\.(\d+)/);
+$VERSION = sprintf("%d", q$Revision$ =~ /(\d+)/);
 
 # Some lookup tables
 
